6ed4806dc6fc1d45cb749374a6d7c449caa47abc
3markhaled500-blip <3markhaled500@gmail.com>
2026-09-05 22:57:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -261,7 +261,7 @@ client.on(
 
           .setDescription(
 
-            `##  نورت السيرفر يا ${member}\n\n` +
+            `## نورت السيرفر  ${member}\n\n` يا +
 
             "أهلاً وسهلاً بك في **BARAKAT COMMUNITY** ❤️\n\n" +
 
